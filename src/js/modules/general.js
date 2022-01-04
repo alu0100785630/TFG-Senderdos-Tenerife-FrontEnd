842289cddcdf2f2a5fa6a499a86669f4cdfbf20b
aldordar <aldordar@gmail.com>
2022-01-04 21:17:07 +0000

    Fix when there are no reviews

--- src/js/modules/general.js
+++ src/js/modules/general.js
@@ -5,15 +5,20 @@ export const greetingFunction = ()=> {
 export const senderoReviews = () => {
   if (document.querySelector('.sendero-reviews')) {
 
-    $('.sendero-reviews').slick({
-      infinite: true,
-      speed: 800,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      dots: true,
-      prevArrow: '<i class="fas fa-chevron-left prev"></i>',
-      nextArrow: '<i class="fas fa-chevron-right next"></i>',
-    });
+    if (document.querySelectorAll('.sendero-reviews__single-review').length > 3) {
+      $('.sendero-reviews').slick({
+        infinite: true,
+        speed: 800,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: '<i class="fas fa-chevron-left prev"></i>',
+        nextArrow: '<i class="fas fa-chevron-right next"></i>',
+      });
+    }
+    else {
+      document.querySelector('.sendero-reviews').classList.add('d-flex');
+    }
 
   }
 }
